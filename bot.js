7e0df60a55c49aecc81ed1a4a05f0edcbb993757
OKEPlazmA <okeplazma@gmail.com>
2017-11-22 22:46:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', {} => {
-  console.log('I am ready');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-  if (message.content === '.ping') {
-    message.reply('pong');
-    }
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-client.login(process.env.MzM5MjIwMjU4MDI2MTYwMTMx.DPfeng.z84V4nTl8YwPmmuPzk-6EmfoUjY);
+client.login('MzM5MjIwMjU4MDI2MTYwMTMx.DPfeng.z84V4nTl8YwPmmuPzk-6EmfoUjY');
